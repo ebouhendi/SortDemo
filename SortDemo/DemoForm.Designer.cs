5f2e82b26fed884c6443a628e5d50474e8debf92
Ehsan <ehsan.bouhendi@gmail.com>
2015-12-07 12:40:58 +0100

    Project initial commit.

---
+++ SortDemo/DemoForm.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace SortDemo
+{
+    partial class DemoForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.bubbleSortPanel = new System.Windows.Forms.Panel();
+            this.insertionSortPanel = new System.Windows.Forms.Panel();
+            this.quickSortPanel = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // bubbleSortPanel
+            // 
+            this.bubbleSortPanel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.bubbleSortPanel.Location = new System.Drawing.Point(12, 12);
+            this.bubbleSortPanel.Name = "bubbleSortPanel";
+            this.bubbleSortPanel.Size = new System.Drawing.Size(261, 301);
+            this.bubbleSortPanel.TabIndex = 0;
+            this.bubbleSortPanel.Paint += new System.Windows.Forms.PaintEventHandler(this.bubbleSortPanel_Paint);
+            // 
+            // insertionSortPanel
+            // 
+            this.insertionSortPanel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.insertionSortPanel.Location = new System.Drawing.Point(279, 12);
+            this.insertionSortPanel.Name = "insertionSortPanel";
+            this.insertionSortPanel.Size = new System.Drawing.Size(261, 301);
+            this.insertionSortPanel.TabIndex = 1;
+            this.insertionSortPanel.Paint += new System.Windows.Forms.PaintEventHandler(this.insertionSortPanel_Paint);
+            // 
+            // quickSortPanel
+            // 
+            this.quickSortPanel.Location = new System.Drawing.Point(546, 12);
+            this.quickSortPanel.Name = "quickSortPanel";
+            this.quickSortPanel.Size = new System.Drawing.Size(261, 301);
+            this.quickSortPanel.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(674, 530);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // DemoForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(825, 609);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.quickSortPanel);
+            this.Controls.Add(this.insertionSortPanel);
+            this.Controls.Add(this.bubbleSortPanel);
+            this.Name = "DemoForm";
+            this.Text = "Sort Demo";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel bubbleSortPanel;
+        private System.Windows.Forms.Panel insertionSortPanel;
+        private System.Windows.Forms.Panel quickSortPanel;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ SortDemo/DemoForm.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SortDemo
+{
+    public partial class DemoForm : Form
+    {
+        private const int ArrayLength = 50;
+        private int[] array = new int [ArrayLength];
+        private ArrayRepresenter bubble_array;
+        private ArrayRepresenter insertion_array;
+        private int[] quick_array;
+
+        private class ArrayRepresenter
+        {
+            public readonly int[] array;
+            public int swaping1;
+            public int swaping2;
+            public int Length { get { return array.Length; } }
+
+            public ArrayRepresenter(int[] array)
+            {
+                this.array = array;
+            }
+
+            public void swap( int idx1,  int idx2)
+            {
+                var temp = array[idx1];
+                array[idx1] = array[idx2];
+                array[idx2] = temp;
+
+                swaping1 = idx1;
+                swaping2 = idx2;
+            }
+        }
+        public DemoForm()
+        {
+            InitializeComponent();
+
+            
+            for (int i = 0; i< array.Length; i++)
+            {
+                array[i] = i+1;
+            }
+
+            var random = new Random();
+
+            for (int i = 0; i < array.Length * 4; i++)
+            {
+                var next = random.Next(0, array.Length);
+                var temp = array[0];
+                array[0] = array[next];
+                array[next] = temp;
+            }
+
+            
+            quick_array = (int[])array.Clone();
+
+        }
+
+        private void insertionSortPanel_Paint(object sender, PaintEventArgs e)
+        {
+            RenderBars(e.Graphics, insertion_array, insertionSortPanel.Width);
+        }
+
+        private static void RenderBars(Graphics gr, ArrayRepresenter representer, int width)
+        {
+            if(representer == null)
+            {
+                return;
+            }
+
+            for (int i = 0; i < representer.array.Length; i++)
+            {
+                if(i == representer.swaping1)
+                {
+                    gr.FillRectangle(Brushes.Yellow, 0, i * 6, representer.array[i] * (width / representer.array.Length), 4);
+                    continue;
+                }
+
+                if (i == representer.swaping2)
+                {
+                    gr.FillRectangle(Brushes.YellowGreen, 0, i * 6, representer.array[i] * (width / representer.array.Length), 4);
+                    continue;
+                }
+
+                    gr.FillRectangle(Brushes.OrangeRed, 0, i * 6, representer.array[i] * (width / representer.array.Length), 4);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            insertion_array = new ArrayRepresenter((int[])array.Clone());
+            bubble_array = new ArrayRepresenter((int[])array.Clone());
+            var t1 = Task.Run(() =>
+            {
+                for (int i = 0; i < insertion_array.Length; i++)
+                {
+                   for (int j = i; j < insertion_array.Length; j++)
+                   {
+                       if (insertion_array.array[i] > insertion_array.array[j])
+                       {
+                            insertion_array.swap(i, j);
+                           System.Threading.Thread.Sleep(20);
+                        }
+                       
+                   }
+               }
+            });
+
+            var t2 = Task.Run(() =>
+            {
+                var swapped = true;
+                while(swapped)
+                {
+                    swapped = false;
+                    for (int j = 1; j < bubble_array.Length; j++)
+                    {
+                        if (bubble_array.array[j] < insertion_array.array[j-1])
+                        {
+                            bubble_array.swap(j, j-1);
+                            swapped = true;
+                            System.Threading.Thread.Sleep(20);
+                        }
+
+                    }
+                }
+            });
+
+
+            while (!t1.IsCompleted || !t2.IsCompleted)
+            {
+              insertionSortPanel.Refresh();
+              bubbleSortPanel.Refresh();
+              System.Threading.Thread.Sleep(10);
+            }
+            
+        }
+
+        
+
+        private void bubbleSortPanel_Paint(object sender, PaintEventArgs e)
+        {
+            RenderBars(e.Graphics, bubble_array, bubbleSortPanel.Width);
+        }
+    }
+}
